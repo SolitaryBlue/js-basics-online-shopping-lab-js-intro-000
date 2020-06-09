b88a29590026b6b7f8095712fe6388af8bae318a
JP Luttrell <jpluttrell@protonmail.com>
2020-06-09 00:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,25 +44,9 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  var found = false;
-  if(cart.length === 0) {
-    return "That item is not in your cart.";
-  } else {
-    for ( var i=0; i < cart.length; i++) {
-      console.log( "item:"+item+"carti:"+cart[i].itemName);
-      if (cart[i].itemName === item) {
-        found = true;
-        console.log("splicing");
-        cart.splice(i, 1);
-      }
-    }
+  if(cart.length == 0) {
+    return `That item is not in your cart.`
   }
-    if (found === false) {
-      return cart;
-    } else {
-      return "That item is not in your cart.";
-    }
-
 }
 
 function placeOrder(cardNumber) {
